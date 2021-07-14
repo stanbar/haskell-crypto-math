d1720317f2264929f6ecd7fbb9376598b14e44f8
Stanislaw Baranski <stan@stan.bar>
2021-07-14 11:42:39 +0200

    test: add totient properites tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,4 @@
-import Lib (pow', sqrt', gcd', divisors, nthPrime, nPrimes, relativePrimes, totient, isRelativePrime)
+import Lib (divisors, gcd', isRelativePrime, nPrimes, nthPrime, pow', relativePrimes, sqrt', totient)
 
 main :: IO ()
 main = do
@@ -10,16 +10,21 @@ main = do
 
   putStrLn $ if gcd' 12 8 == 4 then "Ok" else "False"
 
-  putStrLn $ if divisors 12 == [4,3,6,2,1,12] then "Ok" else "False"
+  putStrLn $ if divisors 12 == [4, 3, 6, 2, 1, 12] then "Ok" else "False"
 
   putStrLn $ if nthPrime 20 == 71 then "Ok" else "False"
 
-  putStrLn $ if nPrimes 20 == [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71] then "Ok" else "False"
+  putStrLn $ if nPrimes 20 == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71] then "Ok" else "False"
 
   putStrLn $ if not $ isRelativePrime 9 6 then "Ok" else "False"
 
   putStrLn $ if isRelativePrime 9 7 then "Ok" else "False"
 
-  putStrLn $ if relativePrimes 20 == [1,3,7,9,11,13,17,19] then "Ok" else "False"
+  putStrLn $ if relativePrimes 20 == [1, 3, 7, 9, 11, 13, 17, 19] then "Ok" else "False"
 
   putStrLn $ if totient 20 == 8 then "Ok" else "False"
+
+  putStrLn $ if isRelativePrime 9 7 == (totient (9 * 7) == totient 9 * totient 7) then "Ok" else "False"
+  putStrLn $ if isRelativePrime 9 6 == (totient (9 * 6) == totient 9 * totient 6) then "Ok" else "False"
+
+  return ()
